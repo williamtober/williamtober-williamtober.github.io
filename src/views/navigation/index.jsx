05ac2ec8fe16f9414e90afe40cf9ab96bec95954
William Tober <wvtober@student.fullsail.edu>
2023-05-29 13:33:06 -0400

    navigation change, made icons contain to rest of site

--- src/views/navigation/index.jsx
+++ src/views/navigation/index.jsx
@@ -25,19 +25,22 @@ const Navigation = () => {
 
     return (
         <nav className="w-full top-0 left-0 fixed z-10 h-20 flex flex-row items-center justify-center md:justify-end px-10 bg-white dark:bg-gray-950 shadow-md">
-            <ul className="flex flex-row items-center gap-x-14 md:gap-x-4">
-                {
-                    socials.map((social) => {
-                        console.log(social);
-                        return (
-                            <li className={"h-20 aspect-square " + centering}>
-                                <a className={"h-full w-full " + centering} href={social.link}>
-                                    <EButton type="icon" icon={social.icon} />
-                                </a>
-                            </li>
-                        )
-                    })
-                }
+            <ul className="flex w-full max-w-screen-xl flex-row items-center md:justify-end justify-center mx-auto gap-x-14 md:gap-x-4">
+                
+                    {
+                        socials.map((social) => {
+                            console.log(social);
+                            return (
+                                <li className={"h-20 aspect-square " + centering}>
+                                    <a className={"h-full w-full " + centering} href={social.link}>
+                                        <EButton type="icon" icon={social.icon} />
+                                    </a>
+                                </li>
+                            )
+                        })
+                    }
+                
+                
 
             </ul>
         </nav>
