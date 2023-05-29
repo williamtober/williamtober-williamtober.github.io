1d75ce44e2493b5a6ddfd92caef6d491813fedf1
William Tober <wvtober@student.fullsail.edu>
2023-05-29 13:02:09 -0400

    switch to dev and start organizing

--- src/views/projects/index.jsx
+++ src/views/projects/index.jsx
@@ -9,6 +9,38 @@ import acuteDark from "../../assets/acute_dark.png";
 
 
 const Projects = () => {
+
+    const projects = [
+        {
+            name: "Pokeo",
+            description: "Trading Card Game Management Application",
+            link: "https://pokeoapp.com",
+            icon: "network-wired",
+            technologies: [
+                {
+                    name: "HTML5",
+                    icon: "html5"
+                },
+                {
+                    name: "CSS3",
+                    icon: "css3"
+                },
+                {
+                    name: "JavaScript",
+                    icon: "javascript"
+                },
+                {
+                    name: "Tailwind CSS",
+                    icon: "tailwind"
+                },
+                {
+                    name: "TypeScript",
+                    icon: "typescript"
+                }
+            ]
+        }
+    ]
+    
     return (
         <section id="projects" class="bg-white dark:bg-gray-900 antialiased">
             <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
@@ -163,7 +195,7 @@ const Projects = () => {
                                 
                             </div>
 
-                            <a href="#" title=""
+                            <a href="https://github.com/williamtober/daisy" title=""
                                 class="text-white bg-primary justify-center hover:bg-secondary inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-secondary dark:hover:bg-primary dark:focus:ring-secondary"
                                 role="button">
                                 View Codebase
@@ -227,7 +259,7 @@ const Projects = () => {
                                 </div>
                             </div>
 
-                            <a href="#" title=""
+                            <a href="https://github.com/williamtober/acute-design-system" title=""
                                 class="text-white bg-primary justify-center hover:bg-secondary inline-flex items-center focus:ring-4 focus:outline-none focus:ring-primary font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-secondary dark:hover:bg-primary dark:focus:ring-secondary"
                                 role="button">
                                 View documentation
